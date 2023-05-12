14cfc79d95b0c6533e62ccb651e49cc5985203df
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-05-12 16:21:38 -0400

    revision

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (process.env.NODE_ENV === "production") disableReactDevTools();
+// if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
